53181f568d3d4b0a1a809f3e644f8ff26b38b87f
Colin Seybold <cpseybold@gmail.com>
2022-12-11 14:57:09 -0500

    Add files via upload

--- Encoder_Both_Working.ino
+++ Encoder_Both_Working.ino
@@ -4,22 +4,24 @@
 // Encoder output to Arduino Interrupt pin. Tracks the pulse count, is a static value
 #define ENC_IN_RIGHT_A 2
 #define ENC_IN_LEFT_A 3
+
 // Other encoder output to Arduino to keep track of wheel direction, is a static value
 // Tracks the direction of rotation.
 #define ENC_IN_RIGHT_B 4
 #define ENC_IN_LEFT_B 8
 
-
- 
 // True = Forward; False = Reverse
 boolean Direction_right = true;
 boolean Direction_left = true;
+
 // Keep track of the number of right wheel pulses
 volatile long right_wheel_pulse_count = 0;
 volatile long left_wheel_pulse_count = 0; 
+
 // half second interval for measurements in milliseconds
 int interval = 500;
 const int divider = 60/(interval/1000);
+
 // Counters for milliseconds during interval
 long previousMillis = 0;
 long currentMillis = 0;
@@ -27,6 +29,7 @@ long currentMillis = 0;
 // Variable for RPM measuerment
 float rpm_right = 0;
 float rpm_left = 0;
+
  //Constants for conversions
 const float rpm_to_radians = 0.10471975512;
 const float rad_to_deg = 57.29578;
@@ -43,6 +46,7 @@ void setup() {
   //This helps reduce the noise in the signal
   pinMode(ENC_IN_RIGHT_B , INPUT_PULLUP);
   pinMode(ENC_IN_LEFT_B , INPUT_PULLUP);
+
   // Every time the pin goes high, this is a pulse, this allows the pulses to be counted, gets reset after time period is over
   attachInterrupt(digitalPinToInterrupt(ENC_IN_LEFT_A), left_wheel_pulse, RISING);
   attachInterrupt(digitalPinToInterrupt(ENC_IN_RIGHT_A), right_wheel_pulse, RISING);
@@ -62,6 +66,7 @@ void loop() {
     // Calculate rpm
     rpm_right = (float)(right_wheel_pulse_count * 120 / ENC_COUNT_REV);
     rpm_left = (float)(left_wheel_pulse_count * 120 / ENC_COUNT_REV);
+    
     //ang_velocity_right = rpm_right * rpm_to_radians;   
     //ang_velocity_right_deg = ang_velocity_right * rad_to_deg;
     Serial.print(" Right Pulses: ");
@@ -98,13 +103,12 @@ void right_wheel_pulse() {
   if (Direction_right) {
     right_wheel_pulse_count++;
   }
- /* else {
-    right_wheel_pulse_count--;
-  }*/
+
 }
 void left_wheel_pulse() {
    
   // Read the value for the encoder for the right wheel
+  int val = digitalRead(ENC_IN_LEFT_B);
 
   if(val == LOW) {
     Direction_left = true;
@@ -118,8 +122,5 @@ void left_wheel_pulse() {
   if (Direction_left) {
     left_wheel_pulse_count++;
   }
-  /*else {
-    right_wheel_pulse_count--;
-  }
-  */
+
 }
--- IAB Test.ino
+++ IAB Test.ino
@@ -5,9 +5,13 @@
 #define motor1pin2 10
 #define motor2pin1 11
 #define motor2pin2 12
+
 // Keep track of the number of right wheel pulses
 volatile long right_wheel_pulse_count = 0;
+
+//Count to only let it break once
 int count = 0;
+
 void setup() {
 // Open the serial port at 9600 bps, you use this to output info from arduino
   Serial.begin(9600); 
@@ -19,19 +23,22 @@ void setup() {
   pinMode(motor2pin2, OUTPUT);
 // Every time the pin goes high, this is a pulse, this allows the pulses to be counted, gets reset after time period is over
   attachInterrupt(digitalPinToInterrupt(ENC_IN_RIGHT_A), right_wheel_pulse, RISING);
-//Releases break
 
+//Pulls brake in
   digitalWrite(motor1pin1, LOW);
   digitalWrite(motor1pin2, HIGH);
   digitalWrite(motor2pin1, LOW);
   digitalWrite(motor2pin2, HIGH);
+//5 second delay
 delay(5000);
-
+//Releases Brake
   digitalWrite(motor1pin1, HIGH);
   digitalWrite(motor1pin2, LOW);
   digitalWrite(motor2pin1, HIGH);
   digitalWrite(motor2pin2, LOW);
+// 2 second delay
   delay(2000);
+//Stops signal to actuator
   digitalWrite(motor1pin1, LOW);
   digitalWrite(motor2pin1, LOW);
 }
@@ -40,11 +47,14 @@ void loop() {
 // After 2 Rotations brakes will be applied
   if(right_wheel_pulse_count>36&&count==0)
   {
+    //Brake Wheel 
     digitalWrite(motor1pin1, LOW);
     digitalWrite(motor1pin2, HIGH);
     digitalWrite(motor2pin1, LOW);
     digitalWrite(motor2pin2, HIGH);
+    //Delay for 2 seconds
     delay(2000);
+    //Stops signal to actuator    
     digitalWrite(motor1pin2, LOW);
     digitalWrite(motor2pin2, LOW);
     count++;
